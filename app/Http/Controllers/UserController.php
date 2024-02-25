fe58ca6d38e4c649885f16af117f58a1fd88772a
コモディ <nobu54459@gmail.com>
2024-02-25 16:05:44 +0900

    user_put実装 storeactionからshowactionに修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,21 +6,28 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\User\ShowRequest;
 use App\Http\Resources\UserResource;
-use App\UseCases\User\StoreAction;
+use App\UseCases\User\ShowAction;
+use App\UseCases\User\UpdateAction;
 
 
 class UserController extends Controller
 {
     //
     protected $storeAction;
+    protected $updateAction;
 
-    public function __construct(StoreAction $storeAction)
+    public function __construct(ShowAction $showAction, UpdateAction $updateAction)
     {
-        $this->storeAction = $storeAction;
+        $this->showAction = $showAction;
+        $this->updateAction = $updateAction;
     }
 
     public function show(Request $request, $userId)
     {
-        return $this->storeAction->execute($userId);
+        return $this->showAction->execute($userId);
+    }
+    public function update(Request $request, $userId)
+    {
+        return $this->updateAction->execute($userId);
     }
 }
--- app/UseCases/User/UpdateAction.php
+++ app/UseCases/User/UpdateAction.php
@@ -5,11 +5,11 @@ namespace App\UseCases\User;
 use App\Models\User;
 use App\Http\Resources\UserResource;
 
-class StoreAction
+class UpdateAction
 {
     public function execute($userId)
     {
-        $user = User::find($userId);
+        $user = User::update($userId);
 
         return new UserResource($user);
     }
